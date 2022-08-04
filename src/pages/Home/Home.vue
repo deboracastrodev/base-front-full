4d6fd1003b2042f34f59e804a9c3e82df25eba44
Debora Castro <debora@vert-capital.com>
2022-08-04 17:36:00 -0300

    feat: ajutes de layout

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>Content home</h1>
-  <v-table :rows="rows" :cols="cols"></v-table>
+  <h1>Página Home</h1>
+  <div style="width: 50%; padding-top: 50px;">
+    <h4>Componente tabela importando da lib vert-frame-core</h4>
+    <label>Essa lib foi criada inspirada no design system da VERT</label>
+    <v-table :rows="rows" :cols="cols" style="padding-top: 20px;"> </v-table>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -15,8 +19,8 @@ export default defineComponent({
     const cols = ['id', 'name', 'email', 'phone'];
     const rows = [
       { id: 1, name: 'John', email: 'jonh@teste.com', phone: '+5511999999999' },
-      { id: 2, name: 'Jane', email: 'jane@teste.com', phone: '+5511999999999' },
-    ]
+      { id: 2, name: 'Jane', email: 'jane@teste.com', phone: '+5511999999999' }
+    ];
     return { cols, rows };
   }
 });
